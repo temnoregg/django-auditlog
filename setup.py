c5678fcfca1cf898f726df0b719d696cfe4eceef
Petr Chalupny <temnoregg@gmail.com>
2016-02-28 21:13:03 +0100

    Add trying to run with Django < 1.5

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     author_email='janjelle@jjkester.nl',
     description='Audit log app for Django',
     install_requires=[
-        'Django>=1.7',
+        'Django<1.5',
         'django-jsonfield>=0.9.13',
     ]
 )
